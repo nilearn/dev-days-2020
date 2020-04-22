0823bc776e9ac683a9e34b5d77a09fb307c2bf1e
Jerome Dockes <jerome@dockes.org>
2020-04-21 20:12:14 -0400

    detail

--- add_cet.sh
+++ add_cet.sh
@@ -4,7 +4,7 @@ awk -i inplace '
 BEGIN {FS="|"}
 ($1 ~ /EDT/) && ! ($2 ~ /CET/) {timetable = 1; sub($1, $1 "| CET ")}
 /^ *$/ {timetable = 0}
-/^[-|]+$/ && timetable == 1 {sub("-", "----|--")}
+/^[| ]*--[-| ]+$/ && timetable == 1 {sub("-", "----|--")}
 $1 ~ /[0-9][0-9]:[0-9][0-9]/ && timetable == 1 {
     "TZ=CET date -d \"2020-05-19 "$1" EDT\" +\""$1"| %R \"" | getline d ;
     sub($1, d);
